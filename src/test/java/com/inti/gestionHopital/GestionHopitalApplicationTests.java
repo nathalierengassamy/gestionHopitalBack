c3fa251527aebd5f4885a942a8fabd0f919da69c
nathalierengassamy <nathalierengassamy@outlook.fr>
2022-06-07 16:20:44 +0200

    debut projet

---
+++ src/test/java/com/inti/gestionHopital/GestionHopitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inti.gestionHopital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionHopitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
